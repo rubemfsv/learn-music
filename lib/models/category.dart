8c40abc79ac67c49e69f19dd5e57fc22913cf1d1
Rubem <rfsv@ic.ufal.br>
2020-07-26 11:17:26 -0300

    Start using MobX

---
+++ lib/models/category.dart
@@ -0,0 +1,30 @@
+import 'package:mobx/mobx.dart';
+import 'package:uuid/uuid.dart';
+
+class Category = _Category with _$Category;
+
+abstract class _Category with Store {
+  _Category({String id, this.description = ''})
+      : id = id ?? Uuid().generateV4();
+
+  final String id;
+
+  @observable
+  String description;
+
+  @override
+  String toString() {
+    return '_Category{id: $id, description: $description}';
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is _Category &&
+          runtimeType == other.runtimeType &&
+          id == other.id &&
+          description == other.description;
+
+  @override
+  int get hashCode => id.hashCode ^ description.hashCode;
+}
